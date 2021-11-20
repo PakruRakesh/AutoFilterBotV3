df8c47ac3c5768519ff2fe0af09d1d45e3219913
ɢx  ₪ᴇᴏ <89633118+GxNeo@users.noreply.github.com>
2021-11-20 21:40:40 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -117,7 +117,7 @@ async def start(bot, message):
                 username = None if not message.from_user.username else '@' + message.from_user.username,
                 mention = message.from_user.mention,
                 id = message.from_user.ɪᴅ
-            ]
+            )
              
         reply_markup=InlineKeyboardMarkup(
                 [[
